9e0cd5f8fb2096c4d3449da63d2e42e351200cfd
leelagithubrepo <leela2771978@gmail.com>
2017-10-22 07:56:28 -0700

    The changes are updated and checked in here
    The changes are updated and checked in here

--- GlobalActivePower-Plot2.R
+++ GlobalActivePower-Plot2.R
@@ -16,7 +16,7 @@ powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
 png("plot2.png", width=480, height=480)
 
-## Plot 2
+# Plot 2
 plot(x = powerDT[, dateTime]
      , y = powerDT[, Global_active_power]
      , type="l", xlab="", ylab="Global Active Power (kilowatts)")
--- GlobalActivePower-Plot4.R
+++ GlobalActivePower-Plot4.R
@@ -34,7 +34,7 @@ legend("topright", col=c("black","red","blue")
        , bty="n"
        , cex=.5) 
 
-# Plot 4
+# Plot4
 plot(powerDT[, dateTime], powerDT[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
